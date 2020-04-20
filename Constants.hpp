fcc8bc295a5b765c80b8cf90b388b89731e00c77
kolusask <19ak97@gmail.com>
2020-04-20 23:07:31 +0200

    Added Constants header

---
+++ Constants.hpp
@@ -0,0 +1,5 @@
+#define CROSSOVER_POINTS        2
+#define CROSSOVER_PROB          0.63
+#define MUTATION_PROB           0.0037
+#define NUMBER_OF_GENERATIONS   500
+#define POPULATION_SIZE         10
