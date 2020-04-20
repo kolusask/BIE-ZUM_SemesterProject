ca37596c8ee6afa5c50bcee6524b2ea65cd5ac9e
kolusask <19ak97@gmail.com>
2020-04-20 23:24:22 +0200

    Renamed Chromosome class into Individual

--- Individual.hpp
+++ Individual.hpp
@@ -7,28 +7,28 @@
 #include <random>
 #include <vector>
 
-class Chromosome;
+class Individual;
 
-using Children = std::pair<Chromosome, Chromosome>;
+using Children = std::pair<Individual, Individual>;
 using Genome = std::vector<bool>;
 
-class Chromosome {
+class Individual {
   public:
     /// Constructor from genome size
-    Chromosome(const size_t size = s_GenomeSize);
+    Individual(const size_t size = s_GenomeSize);
 
-    /// Create a chromosome with random genome
-    static Chromosome random(const size_t size);
+    /// Create a Individual with random genome
+    static Individual random(const size_t size);
 
-    /// Produce a chromosome from two parents (using crossover and mutation)
-    Children operator+(const Chromosome& other) const;
+    /// Produce a Individual from two parents (using crossover and mutation)
+    Children operator+(const Individual& other) const;
 
     /// Calculate fitness from genome using passed function
     size_t fitness(const std::function<size_t(const Genome&)>& fitFun);
 
   private:
     bool operator[](const size_t i) const;
-    static Children crossover(const Chromosome& p1, const Chromosome& p2);
+    static Children crossover(const Individual& p1, const Individual& p2);
     void mutate();
 
     Genome m_Genome;
@@ -36,34 +36,34 @@ class Chromosome {
     static size_t s_GenomeSize;
 };
 
-size_t Chromosome::s_GenomeSize = 0;
+size_t Individual::s_GenomeSize = 0;
 
-Chromosome::Chromosome(const size_t size) {
+Individual::Individual(const size_t size) {
     m_Genome.reserve(size);
     s_GenomeSize = size;
 }
 
-Chromosome Chromosome::random(const size_t size) {
-    Chromosome result(size);
+Individual Individual::random(const size_t size) {
+    Individual result(size);
     for (size_t i = 0; i < size; i++)
         result.m_Genome.push_back(Random::generate().next_bool());
     return std::move(result);
 }
 
-Children Chromosome::operator+(const Chromosome& other) const {
+Children Individual::operator+(const Individual& other) const {
     Children children = crossover(*this, other);
     children.first.mutate();
     children.second.mutate();
     return std::move(children);
 }
 
-size_t Chromosome::fitness(const std::function<size_t(const Genome&)>& fitFun) {
+size_t Individual::fitness(const std::function<size_t(const Genome&)>& fitFun) {
     return fitFun(m_Genome);
 }
 
-bool Chromosome::operator[](const size_t i) const { return this->m_Genome[i]; }
+bool Individual::operator[](const size_t i) const { return this->m_Genome[i]; }
 
-Children Chromosome::crossover(const Chromosome& p1, const Chromosome& p2) {
+Children Individual::crossover(const Individual& p1, const Individual& p2) {
 #if CROSSOVER_POINTS > 0    
     if (Random::generate().next_bool(CROSSOVER_PROB)) {
         size_t points[CROSSOVER_POINTS];
@@ -73,16 +73,16 @@ Children Chromosome::crossover(const Chromosome& p1, const Chromosome& p2) {
         for (size_t i = 0; i < CROSSOVER_POINTS - 1; i++) {
             const size_t start = points[i];
             const size_t stop = points[i + 1];
-            const Chromosome& parent = i % 2 ? p2 : p1;
+            const Individual& parent = i % 2 ? p2 : p1;
             for (size_t j = start; j < stop; j++) {
                 children.first.m_Genome.push_back(parent[i]);
                 children.second.m_Genome.push_back(!parent[i]);
             }
         }
 #if CROSSOVER_POINTS % 2 == 0
-        const Chromosome& parent = p1;
+        const Individual& parent = p1;
 #else
-        const Chromosome& parent = p2;
+        const Individual& parent = p2;
 #endif
         for (size_t i = points[CROSSOVER_POINTS - 1]; i < s_GenomeSize; i++) {
             children.first.m_Genome.push_back(parent[i]);
@@ -94,7 +94,7 @@ Children Chromosome::crossover(const Chromosome& p1, const Chromosome& p2) {
     return Children(p1, p2);
 }
 
-void Chromosome::mutate() {
+void Individual::mutate() {
     for (size_t i = 0; i < s_GenomeSize; i++)
         if (Random::generate().next_bool(MUTATION_PROB))
             m_Genome[i] = !m_Genome[i];
